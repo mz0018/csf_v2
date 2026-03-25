1fc0900f78ad85722fcdd8127b1c8082e1420368
mz0018 <martinezhanzmenzi@gmail.com>
2026-03-25 17:29:36 +0800

    semi production testing

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.mount("/qrcodes", StaticFiles(directory="qrcodes"), name="qrcodes")
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["http://192.168.3.104"],
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://192.168.3.104"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
